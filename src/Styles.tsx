eac4d62dd019d6924c15789526927abaf2444ee6
kennarddh <79390992+kennarddh@users.noreply.github.com>
2025-05-09 00:43:59 +0700

    feat: Change font

--- src/Styles.tsx
+++ src/Styles.tsx
@@ -5,8 +5,8 @@ const GlobalStyle = createGlobalStyle`
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
-
-		font-family: "Roboto", sans-serif;
+		
+		font-family: 'Quicksand', sans-serif;
 
 		scroll-behavior: smooth;
 	}
