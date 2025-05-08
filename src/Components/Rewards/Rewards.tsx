8e7fb46f127ab28392536261500403e311b07fa4
kennarddh <79390992+kennarddh@users.noreply.github.com>
2025-05-09 01:34:31 +0700

    feat: Show rewards game button

--- src/Components/Rewards/Rewards.tsx
+++ src/Components/Rewards/Rewards.tsx
@@ -33,6 +33,7 @@ const Rewards: FC = () => {
 	const [PreviousItemsIds, SetPreviousItemsIds] = useState<string[]>([])
 	const [DidStart, SetDidStart] = useState(false)
 	const [IsFading, SetIsFading] = useState(false)
+	const [IsShowing, SetIsShowing] = useState(false)
 
 	const Reroll = useCallback(() => {
 		SetDidStart(true)
@@ -151,62 +152,79 @@ const Rewards: FC = () => {
 		SetPreviousItemsIds([])
 	}, [])
 
+	const Show = useCallback(() => {
+		SetIsShowing(true)
+	}, [])
+
 	return (
 		<SectionContainer id='rewards'>
 			<SectionTitle>Rewards</SectionTitle>
 
-			{DidStart ? (
-				<Image
-					alt="Game's Image"
-					src={
-						Items.find(
-							item =>
-								item.id ===
-								PreviousItemsIds[PreviousItemsIds.length - 1],
-						)?.imageSrc
-					}
-					onLoad={() => {
-						SetIsFading(false)
-					}}
-					onTransitionEnd={() => {
-						SetIsFading(false)
-					}}
-					className={IsFading ? 'faded' : 'visible'}
-				/>
-			) : (
-				<ImagePlaceholder>
-					<Info>Press start to play!</Info>
-				</ImagePlaceholder>
-			)}
-
-			{DidStart ? (
-				PreviousItemsIds.length >= 3 ? (
-					<>
-						<Info>Congratulations! It&apos;s done! 🎉🎉🎉</Info>
-						<Button onClick={Restart}>Restart</Button>
-					</>
-				) : (
-					<>
-						<Info>&nbsp;</Info>
-						<Button onClick={Reroll}>Reroll</Button>
-					</>
-				)
+			{!IsShowing ? (
+				<Button onClick={Show}>Show</Button>
 			) : (
 				<>
-					<Info>&nbsp;</Info>
-					<Button onClick={Reroll}>Start</Button>
+					{DidStart ? (
+						<Image
+							alt="Game's Image"
+							src={
+								Items.find(
+									item =>
+										item.id ===
+										PreviousItemsIds[
+											PreviousItemsIds.length - 1
+										],
+								)?.imageSrc
+							}
+							onLoad={() => {
+								SetIsFading(false)
+							}}
+							onTransitionEnd={() => {
+								SetIsFading(false)
+							}}
+							className={IsFading ? 'faded' : 'visible'}
+						/>
+					) : (
+						<ImagePlaceholder>
+							<Info>Press start to play!</Info>
+						</ImagePlaceholder>
+					)}
+
+					{DidStart ? (
+						PreviousItemsIds.length >= 3 ? (
+							<>
+								<Info>
+									Congratulations! It&apos;s done! 🎉🎉🎉
+								</Info>
+								<Button onClick={Restart}>Restart</Button>
+							</>
+						) : (
+							<>
+								<Info>&nbsp;</Info>
+								<Button onClick={Reroll}>Reroll</Button>
+							</>
+						)
+					) : (
+						<>
+							<Info>&nbsp;</Info>
+							<Button onClick={Reroll}>Start</Button>
+						</>
+					)}
+
+					<PreviousImages>
+						{PreviousItemsIds.map((itemId, index) => (
+							<PreviousImage
+								key={index}
+								alt="Game's Image"
+								src={
+									Items.find(item => item.id === itemId)
+										?.imageSrc
+								}
+							/>
+						))}
+					</PreviousImages>
 				</>
 			)}
-
-			<PreviousImages>
-				{PreviousItemsIds.map((itemId, index) => (
-					<PreviousImage
-						key={index}
-						alt="Game's Image"
-						src={Items.find(item => item.id === itemId)?.imageSrc}
-					/>
-				))}
-			</PreviousImages>
 		</SectionContainer>
 	)
 }
